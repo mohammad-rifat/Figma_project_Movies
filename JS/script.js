4a8ff60686d41703d142bab6715d26d9b7d735d8
mohammad-rifat <mohammadrifat7717@gmail.com>
2023-02-07 14:17:29 +0600

    added files & folders

--- JS/script.js
+++ JS/script.js
@@ -11,3 +11,31 @@ $(function () {
     focusOnSelect: true,
   });
 });
+
+/*SCROLL START*/
+let navbar = document.querySelector("#navbar");
+
+window.addEventListener("scroll", function () {
+  let scroll_value = Math.floor(window.scrollY);
+
+  if (scroll_value > 50) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+});
+/*SCROLL END*/
+
+/*MOVE TO TOP START*/
+let move_to_top = document.querySelector(".move_to_top");
+
+window.addEventListener("scroll", function () {
+  let scroll_value = Math.floor(window.scrollY);
+
+  if (scroll_value > 200) {
+    move_to_top.classList.add("move_top");
+  } else {
+    move_to_top.classList.remove("move_top");
+  }
+});
+/*MOVE TO TOP END*/
